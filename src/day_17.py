e13567f677899c6458114b3e3f6f4fc71599b32f
Sam Roussel <smr42.2013@gmail.com>
2023-12-19 23:06:30 -0500

    Day 17...

--- src/day_17.py
+++ src/day_17.py
@@ -1,30 +1,62 @@
 """Advent of code day 17 (part 2)."""
 
-import math
+from collections import defaultdict
 
 
-# node = (i, j, dir, len)
+def add_state(state, cost, grid, visited, queue):
+    i, j, di, dj, length = state
 
-class Dir:
-    North = 0
-    East = 1
-    South = 2
-    West = 3
+    i += di
+    j += dj
+    length += 1
 
-class Node:
-    def __init__(self, i, j, dir, len):
-        self.i = i
-        self.j = j
-        self.dir = dir
-        self.len = len
+    if i not in range(0, len(grid)) or j not in range(0, len(grid[0])):
+        return None
 
-def generate_nodes(node, grid):
-    if (nodelen < 3):
+    cost += int(grid[i][j])
 
+    if i == len(grid) - 1 and j == len(grid[0]) - 1 and length >= 4:
+        return cost
+
+
+    state = (i, j, di, dj, length)
+
+    if state not in visited:
+        visited[state] = cost
+        queue[cost].append(state)
+
+    return None
+
+
+def get_cost(grid):
+    visited = {}
+    queue = defaultdict(list)
+
+    start_right = (0, 0, 0, 1, 0) # i, j, di, dj, len
+    start_down = (0, 0, 1, 0, 0)
+
+    add_state(start_right, 0, grid, visited, queue)
+    add_state(start_down, 0, grid, visited, queue)
+
+    while len(queue):
+        cost = min(queue.keys())
+        
+        for state in queue.pop(min(queue.keys())):
+            i, j, di, dj, length = state
+
+            if length >= 4:
+                if result := add_state((i, j, dj, -di, 0), cost, grid, visited, queue):
+                    return result
+
+                if result := add_state((i, j, -dj, di, 0), cost, grid, visited, queue):
+                    return result
+
+            if length < 10:
+                if result := add_state((i, j, di, dj, length), cost, grid, visited, queue):
+                    return result
 
 
 def run(filename):
     """Return the length of the shortest path."""
     grid = [[x for x in line.strip("\n")] for line in open(filename).readlines()]
-    return len(grid)
-
+    return get_cost(grid)
